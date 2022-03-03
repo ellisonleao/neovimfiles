34b19864fe3e67c4444e6ca8e323d80126941575
Ellison Leão <ellisonleao@gmail.com>
2022-03-03 15:01:56 -0300

    fix treesitter, adding treesitter folding and indent options

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,36 +1,35 @@
 -- module treesiter
-local M = {}
+require("nvim-treesitter.configs").setup({
+  highlight = { enable = true },
+  indent = { enable = true },
+  ensure_installed = {
+    "go",
+    "gomod",
+    "python",
+    "lua",
+    "yaml",
+    "json",
+    "javascript",
+    "bash",
+    "typescript",
+    "hcl",
+  },
+})
 
-function M.config()
-  require("nvim-treesitter.configs").setup({
-    highlight = { enable = true },
-    ensure_installed = {
-      "go",
-      "gomod",
-      "python",
-      "lua",
-      "yaml",
-      "json",
-      "javascript",
-      "bash",
-      "typescript",
-    },
-  })
+-- custom md
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+parser_config.markdown = {
+  install_info = {
+    url = "https://github.com/ikatyang/tree-sitter-markdown",
+    files = { "src/parser.c", "src/scanner.cc" },
+  },
+}
+parser_config.toml = {
+  install_info = {
+    url = "https://github.com/ikatyang/tree-sitter-toml",
+    files = { "src/parser.c", "src/scanner.c" },
+  },
+}
 
-  -- custom md
-  local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-  parser_config.markdown = {
-    install_info = {
-      url = "https://github.com/ikatyang/tree-sitter-markdown",
-      files = { "src/parser.c", "src/scanner.cc" },
-    },
-  }
-  parser_config.toml = {
-    install_info = {
-      url = "https://github.com/ikatyang/tree-sitter-toml",
-      files = { "src/parser.c", "src/scanner.c" },
-    },
-  }
-end
-
-return M
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
