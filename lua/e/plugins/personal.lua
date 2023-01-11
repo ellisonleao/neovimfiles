dccc2cceb7dd6de7c0a1f3b729daecd80dfcd2c5
Ellison Leão <ellisonleao@gmail.com>
2023-01-11 17:55:35 -0300

    do not fetch personal plugins

--- lua/e/plugins/personal.lua
+++ lua/e/plugins/personal.lua
@@ -1,8 +1,9 @@
 return {
-  { dir = "~/code/dotenv.nvim", config = true, cmd = { "Dotenv", "DotenvGet" } },
-  { dir = "~/code/glow.nvim", config = true, cmd = "Glow" },
-  { dir = "~/code/gruvbox.nvim" },
+  { version = false, dir = "~/code/dotenv.nvim", config = true, cmd = { "Dotenv", "DotenvGet" } },
+  { version = false, dir = "~/code/glow.nvim", config = true, cmd = "Glow" },
+  { version = false, dir = "~/code/gruvbox.nvim" },
   {
+    version = false,
     dir = "~/code/carbon-now.nvim",
     config = { options = { theme = "nord", font_family = "JetBrains Mono" } },
     keys = {
