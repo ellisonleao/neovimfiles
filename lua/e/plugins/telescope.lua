df02c5aa00fa4d3a62d8a4ea33f6aa47cbae71e3
Ellison Leão <ellisonleao@gmail.com>
2023-01-11 12:05:32 -0300

    better telescope search

--- lua/e/plugins/telescope.lua
+++ lua/e/plugins/telescope.lua
@@ -1,3 +1,54 @@
+local function get_root()
+  local path = vim.api.nvim_buf_get_name(0)
+  path = path ~= "" and vim.loop.fs_realpath(path) or nil
+  local roots = {}
+  if path then
+    for _, client in pairs(vim.lsp.get_active_clients({ bufnr = 0 })) do
+      local workspace = client.config.workspace_folders
+      local paths = workspace and vim.tbl_map(function(ws)
+        return vim.uri_to_fname(ws.uri)
+      end, workspace) or client.config.root_dir and { client.config.root_dir } or {}
+      for _, p in ipairs(paths) do
+        local r = vim.loop.fs_realpath(p)
+        if path:find(r, 1, true) then
+          roots[#roots + 1] = r
+        end
+      end
+    end
+  end
+  table.sort(roots, function(a, b)
+    return #a > #b
+  end)
+  local root = roots[1]
+  if not root then
+    path = path and vim.fs.dirname(path) or vim.loop.cwd()
+    root = vim.fs.find({".git", "/lua"}, { path = path, upward = true })[1]
+    root = root and vim.fs.dirname(root) or vim.loop.cwd()
+  end
+  return root
+end
+
+-- this will return a function that calls telescope.
+-- cwd will defautlt to lazyvim.util.get_root
+-- for `files`, git_files or find_files will be chosen depending on .git
+local function telescope(builtin, opts)
+  local params = { builtin = builtin, opts = opts }
+  return function()
+    builtin = params.builtin
+    opts = params.opts
+    opts = vim.tbl_deep_extend("force", { cwd = get_root() }, opts or {})
+    if builtin == "files" then
+      if vim.loop.fs_stat((opts.cwd or vim.loop.cwd()) .. "/.git") then
+        opts.show_untracked = true
+        builtin = "git_files"
+      else
+        builtin = "find_files"
+      end
+    end
+    require("telescope.builtin")[builtin](opts)
+  end
+end
+
 return {
   {
     "nvim-telescope/telescope.nvim",
@@ -23,18 +74,8 @@ return {
     },
     requires = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope-ui-select.nvim" },
     keys = {
-      {
-        "<leader>lg",
-        function()
-          require("telescope.builtin").live_grep()
-        end,
-      },
-      {
-        "<leader>ff",
-        function()
-          require("telescope.builtin").find_files({ hidden = true })
-        end,
-      },
+      { "<leader>lg", telescope("live_grep") },
+      { "<leader>ff", telescope("files") },
       {
         "<leader>H",
         function()
