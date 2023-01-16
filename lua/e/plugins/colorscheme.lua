596ee3e587e5a5983690aa49696e92817b31a455
Ellison Leão <ellisonleao@gmail.com>
2023-01-16 15:34:09 -0300

    update colorscheme tag
    - add override for bufferline bug. Ref https://github.com/projekt0n/github-nvim-theme/issues/218

--- lua/e/plugins/colorscheme.lua
+++ lua/e/plugins/colorscheme.lua
@@ -1,11 +1,17 @@
 return {
   {
     "projekt0n/github-nvim-theme",
+    tag = "v0.0.7",
     lazy = false,
     priority = 1000,
     opts = {
       theme_style = "light",
       dark_float = true,
+      overrides = function()
+        return {
+          BufferLineBackground = {},
+        }
+      end,
     },
     config = function(_, opts)
       require("github-theme").setup(opts)
