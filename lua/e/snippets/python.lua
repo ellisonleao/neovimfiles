5a223dcbb7038fc7b9e4674b51b83daf8b0e35e2
Ellison Leão <ellisonleao@gmail.com>
2023-01-23 21:56:07 -0300

    moving more plugins configs to opts/configs.
    - adding cmp-git and octo.nvim

---
+++ lua/e/snippets/python.lua
@@ -0,0 +1,23 @@
+local ls = require("luasnip")
+local s = ls.s
+local i = ls.i
+local fmt = require("luasnip.extras.fmt").fmt
+-- local c = ls.choice_node
+-- local t = ls.text_node
+
+return {
+  s("im", fmt([[import "{}"]], { i(1) })),
+  s(
+    "ifm",
+    fmt(
+      [[
+def main():
+  {}
+
+if __name__ == "__main__":
+  main()
+]],
+      { i(1) }
+    )
+  ),
+}
