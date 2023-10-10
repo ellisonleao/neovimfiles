aa511a9fcbbf19d28af057e23077f3a9962007a4
Ellison Leão <ellisonleao@gmail.com>
2023-10-09 22:58:26 -0300

    fix lsp progress autocmd

--- lua/e/plugins/lualine.lua
+++ lua/e/plugins/lualine.lua
@@ -69,7 +69,8 @@ return {
       require("lualine").setup(opts)
       -- listen lsp-progress event and refresh lualine
       vim.api.nvim_create_augroup("lualine_augroup", { clear = true })
-      vim.api.nvim_create_autocmd("User LspProgressStatusUpdated", {
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "LspProgressStatusUpdated",
         group = "lualine_augroup",
         callback = require("lualine").refresh,
       })
